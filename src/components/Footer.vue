7abc1b99db92c7a61f39d3310b84c1bc7a22a2c3
Martin Stewart <m11t@mm.st>
2026-03-14 06:23:09 +0000

    image alt tag change

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
 	<footer class="footer">
-        <img src="//nuvmo.github.io/media/img/old-dude.jpg" alt="bancm logo" /> a <a href="//nuvmo.github.io">Nuvmo</a> project
+        <img src="//nuvmo.github.io/media/img/old-dude.jpg" alt="Nuvmo logo" /> a <a href="//nuvmo.github.io">Nuvmo</a> project
 	</footer>
 </template>
